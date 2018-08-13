36191ef1c1fb77df5f759620bcc1e7079f20c291
Somesh <mail@somesh.info>
2018-08-13 14:33:39 +0530

    Added Devise Installation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  include Clearance::User
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
   # single user can have many bookings. 
   has_many :bookings
 end
